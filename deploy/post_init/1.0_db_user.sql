ad0222be2c4a78fd05505515d063a05fbf1aad41
无影流asdy <1164752301@qq.com>
2021-12-08 15:32:00 -0800

    更新建库脚本

--- deploy/post_init/1.0_db_user.sql
+++ deploy/post_init/1.0_db_user.sql
@@ -1,18 +1,11 @@
--- we don't know how to generate root <with-no-name> (class Root) :(
-grant super on *.* to 'mysql.session'@localhost;
-
-grant alter, alter routine, create, create routine, create tablespace, create temporary tables, create user, create view, delete, drop, event, execute, file, index, insert, lock tables, process, references, reload, replication client, replication slave, select, show databases, show view, shutdown, super, trigger, update, grant option on *.* to root;
-
-grant alter, alter routine, create, create routine, create tablespace, create temporary tables, create user, create view, delete, drop, event, execute, file, index, insert, lock tables, process, references, reload, replication client, replication slave, select, show databases, show view, shutdown, super, trigger, update, grant option on *.* to root@localhost;
-
 create table group_info
 (
     id bigint auto_increment
         primary key,
     group_name varchar(64) null,
     group_attribute int null,
-    member1_name varchar(64) null,
-    member2_name varchar(64) null,
+    a_name varchar(64) null,
+    b_name varchar(64) null,
     last_update date null,
     constraint table_name_group_name_uindex
         unique (group_name)
@@ -35,5 +28,6 @@ create table relation
     user_id varchar(255) not null,
     group_id bigint not null
 );
+
 
 
