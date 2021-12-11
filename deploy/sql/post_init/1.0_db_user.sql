35cec8cb80ab0ba4f3954bfc50db75c769f1c358
无影流asdy <1164752301@qq.com>
2021-12-11 02:22:25 -0800

    1

--- deploy/sql/post_init/1.0_db_user.sql
+++ deploy/sql/post_init/1.0_db_user.sql
@@ -1,41 +1,45 @@
-create table address
+create table if not exists address
 (
-    ip varchar(64) null,
-    port int null,
-    id bigint auto_increment
+    ip   varchar(64) null,
+    port int         null,
+    id   bigint auto_increment
         primary key
 );
 
-create table group_info
+create table if not exists group_info
 (
-    id bigint auto_increment
+    id              bigint auto_increment
         primary key,
-    group_name varchar(64) null,
-    group_attribute int null,
-    a_name varchar(64) null,
-    b_name varchar(64) null,
-    last_update timestamp null,
+    group_name      varchar(64) null,
+    group_attribute int         null,
+    a_name          varchar(64) null,
+    b_name          varchar(64) null,
+    last_update     timestamp   null,
     constraint table_name_group_name_uindex
         unique (group_name)
 );
 
-create table message
+create table if not exists message
 (
-    id bigint auto_increment
+    id          bigint auto_increment
         primary key,
-    group_id bigint not null,
-    sender_id varchar(255) not null,
-    message varchar(255) null,
-    create_time timestamp null
+    group_id    bigint       not null,
+    sender_id   varchar(255) not null,
+    message     varchar(255) null,
+    create_time timestamp    null
 );
 
-create table relation
+create table if not exists relation
 (
-    id bigint auto_increment
+    id       bigint auto_increment
         primary key,
-    user_id varchar(255) not null,
-    group_id bigint not null
+    user_id  varchar(255) not null,
+    group_id bigint       not null
 );
+
+
+
+
 
 insert into address (ip, port, id) VALUES ('localhost', 30001, 1);
 insert into address (ip, port, id) VALUES ('localhost', 30002, 2);
